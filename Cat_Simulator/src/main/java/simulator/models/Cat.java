49e900d18f4d8da46a323978b57502c6df4d0e74
S4m0912 <saamuel.quintero@gmail.com>
2025-10-18 01:45:02 -0300

    Agregando atributos y metodos a cat

--- Cat_Simulator/src/main/java/simulator/models/Cat.java
+++ Cat_Simulator/src/main/java/simulator/models/Cat.java
@@ -3,11 +3,16 @@ package main.java.simulator.models;
 import main.java.simulator.behaviors.*;
 
 public abstract class Cat{
-
+    
+    private String name;
     private MeowBehavior meow;
     private MoveBehavior move;
     
-    public Cat(){}
+    public Cat(String name){
+
+        this.name = name;
+
+    }
 
     public void setMeow(MeowBehavior meow){
         this.meow = meow;
@@ -16,4 +21,13 @@ public abstract class Cat{
     public void setMove(MoveBehavior move){
         this.move = move;
     }
+
+    public MeowBehavior getMeowBehavior(){
+        return this.meow;
+    }
+
+    public MoveBehavior getMoveBehavior(){
+        return this.move;
+    }
+
 }
